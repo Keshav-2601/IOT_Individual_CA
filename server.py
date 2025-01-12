2d86d95a767cfb66789bcfa8ef801231c73b4a9c
Keshav-2601 <keshavv857@gmail.com>
2025-01-12 20:56:17 +0000

    securing the url by storing sensitive info in env.

--- server.py
+++ server.py
@@ -24,6 +24,7 @@ server.config["SQLALCHEMY_DATABASE_URI"] = (
     f"mariadb+mariadbconnector://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@"
     f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 )
+
 server.config["JWT_SECRET_KEY"]=os.getenv("JWT_KEY")
 
 jwt = JWTManager(server)
